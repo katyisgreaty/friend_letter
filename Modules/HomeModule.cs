9e5fd8927a1b2f54aec045d18a22f8bf98e95fa2
Katy Daviscourt <katy.daviscourt@gmail.com>
2017-02-06 15:39:44 -0800

    fix coding errors in several files

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -1,4 +1,5 @@
 using Nancy;
+using FriendLetter.Objects;
 
 namespace FriendLetter
 {
@@ -6,43 +7,12 @@ namespace FriendLetter
   {
     public HomeModule()
     {
-      Get["/"] = _ =>
-        "<!DOCTYPE html>" +
-        "<html>" +
-        "<head>" +
-          "<title>Hello Friend!</title>" +
-          "<link rel='stylesheet' href='Content/css/styles.css'>" +
-          "<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>" +
-        "</head>" +
-        "<body>" +
-          "<h1>Hello From Afar</h1>" +
-          "<p>Dear Friend,</p>" +
-          "<p>How are you? I hope that you are having a nice weekend. I'm learning programming while in Iceland! </p>" +
-          "<p>Friend, you would not believe how cold it is here. I should have gone to Hawaii instead.</p>" +
-          "<p>But I like programming a lot, so I've got that going for me. </p>" +
-          "<p>Looking forward to seeing you soon. I'll bring you back a souvenir. </p>" +
-          "<p>Cheers,</p>" +
-          "<p>Travel Enthusiast Jane</p>" +
-          "<p><a href='/favorite_photos' >P.S. Check out my favorite travel photos here.</a></p>" +
-        "</body>" +
-        "</html>";
-      Get["/favorite_photos"] = _ =>
-        "<!DOCTYPE html>" +
-        "<html>" +
-          "<head>" +
-            "<title>Hello Friend!</title>" +
-            "<link rel='stylesheet' href='Content/css/styles.css'>" +
-            "<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>" +
-          "</head>" +
-          "<body>" +
-            "<h1>Favorite Traveling Photos</h1>" +
-            "<ul>" +
-              "<li><img src='Content/img/ice.jpg'/></li>" +
-              "<li><img src='Content/img/surf.jpg'/></li>" +
-              "<li><img src='Content/img/night.jpg'/></li>" +
-            "</ul>" +
-          "</body>" +
-        "</html>";
+      Get["/"] = _ => {
+        LetterVariables myLetterVariables = new LetterVariables();
+        myLetterVariables.SetRecipient("Eric");
+        myLetterVariables.SetSender("John");
+        return View["hello.cshtml", myLetterVariables];
+      };
     }
   }
 }
--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,9 @@
+using System.Collections.Generic;
 using System.IO;
 using Microsoft.AspNet.Builder;
-using Nancy.Owin;
 using Nancy;
+using Nancy.Owin;
+using Nancy.ViewEngines.Razor;
 
 namespace FriendLetter
 {
@@ -17,6 +19,23 @@ namespace FriendLetter
     public string GetRootPath()
     {
       return Directory.GetCurrentDirectory();
+    }
+  }
+  public class RazorConfig : IRazorConfiguration
+  {
+    public IEnumerable<string> GetAssemblyNames()
+    {
+      return null;
+    }
+
+    public IEnumerable<string> GetDefaultNamespaces()
+    {
+      return null;
+    }
+
+    public bool AutoIncludeModelNamespace
+    {
+      get { return false; }
     }
   }
 }
